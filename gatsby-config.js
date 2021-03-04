7d568e43c89c14161b0446abc211be65c4a1ff0b
brettjrea <6890798+brettjrea@users.noreply.github.com>
2021-03-04 16:02:31 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,25 @@
 /** * Configure your Gatsby site with this file. 
 * * See: https://www.gatsbyjs.org/docs/gatsby-config/ */
 
-plugins: [`gatsby-plugin-styled-components`,
-
-`gatsby-plugin-react-helmet`]
+plugins: [
+  {
+    resolve: `gatsby-source-strapi`,
+    options: {
+      apiURL: `http://localhost:1337`,
+      queryLimit: 1000, // Default to 100
+      contentTypes: [`article`, `user`],
+      //If using single types place them in this array.
+      singleTypes: [`home-page`, `contact`],
+      // Possibility to login with a strapi user, when content types are not publically available (optional).
+      loginData: {
+        identifier: "",
+        password: "",
+      },
+    },
+  },
+  `gatsby-plugin-styled-components`,
+  `gatsby-plugin-react-helmet`
+]
 
 
   
